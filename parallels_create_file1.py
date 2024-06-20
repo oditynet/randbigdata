18b1113326e0cca7f40d588a3efaf25ba4f112b1
oditynet <oditynet@gmail.com>
2024-06-20 16:15:22 +0300

    Add files via upload

--- parallels_create_file1.py
+++ parallels_create_file1.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python
+
+#select LEFT(data, 50),email AS LongField_First20chars from accounts;
+
 from multiprocessing import Pool
 import random
 import string
 import time
 import hashlib
-
+import psycopg2
+ 
 def compute_cluster(c):
-    random_bytes(100)#len name file
-    print(random_email(100));
+    conn = psycopg2.connect(dbname="db", user="postgres", password="postgress", host="127.0.0.1")
+    cursor = conn.cursor()
+
+    d=random_bytes(100)#len name file
+    e=random_email(10)
+    try:
+      cursor.execute("INSERT INTO accounts (data, email) VALUES ('{}', '{}')".format(d,e))
+      conn.commit()
+    except psycopg2.IntegrityError as e:
+        conn.rollback()
+        print("dupl")
+    #insert into accounts (data,email) values (01,'CentOS'); 
+    cursor.close()
+    conn.close()
 
 def random_email(n):
-    random.seed(random.randint(1,1000000000))
+    random.seed(random.randint(1,100000000))
     name = ''.join(random.choices(string.ascii_uppercase +
                              string.digits, k=n))
     #print("Name = ",name)
@@ -23,9 +39,9 @@ def random_email(n):
     #print("--- %s seconds STRING---" % (time.time() - start_time))
     #print(len(data))
     
-    fp = open('/tmp/123/files/'+name+'@mail.ru', 'w')
+    #fp = open('/tmp/123/files/'+name+'@mail.ru', 'w')
     #fp.write(data.hex())
-    fp.close
+    #fp.close
     #print(data.hex())
     return name+'@mail.ru';
     
@@ -35,10 +51,10 @@ def random_bytes(n):
                              string.digits, k=n))
     #print("Name = ",name)
     #start_time = time.time()
-    fp = open('/tmp/123/files/'+name+'.txt', 'w')
+    #fp = open('/tmp/123/files/'+name+'.txt', 'w')
     
-    start_time = time.time()
-    data = bytearray(random.randint(0, 255) for i in range(6*1024*1024)) #size file 40kb
+    #start_time = time.time()
+    data = bytearray(random.randint(0, 255) for i in range(6*1024)) #size file 40kb
         #print("--- %s 11111 ---" % (time.time() - start_time))
     #print(len(data.hex()))
         #data = bytearray(random.randint(0, 255) for i in range(5)) #size file 40kb
@@ -48,10 +64,11 @@ def random_bytes(n):
     #print(len(data))
     
     #fp = open('/tmp/123/files/'+name+'.txt', 'w')
-    fp.write(data.hex())
-    print("--- %s 2222---" % (time.time() - start_time))
+    #fp.write(data.hex())
+    #print("--- %s 2222---" % (time.time() - start_time))
     #
-    fp.close
+    #fp.close
+    return data.hex()
     #print(data.hex())
 if __name__=="__main__":
    pool = Pool(8) # run 10 task at most in parallel
